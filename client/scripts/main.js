6dfa7dd18af9430f93eb6dd0cbb50d768109b029
Sumeet Adur <sumeet.adur@ft.com>
2015-12-13 22:06:44 +0000

    Working random word generator

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -11,60 +11,103 @@ document.addEventListener('DOMContentLoaded', () => {
   attachFastClick(document.body);
 
   if (document.documentElement.className === 'enhanced') {
-  	var xmlhttp = new XMLHttpRequest();
-  	xmlhttp.onreadystatechange = function() {
-  	  if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-  	    words = JSON.parse(xmlhttp.responseText);
-  	  }
-  	};
-  	xmlhttp.open('GET', 'words.json', true);
-  	xmlhttp.send();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        words = JSON.parse(xmlhttp.responseText);
+        newRandomGuff();
+      }
+    };
+    xmlhttp.open('GET', 'words.json', true);
+    xmlhttp.send();
   }
 
 });
 
-function replaceRandomGuff() {
-	var wordArray = Object.keys(words);
+function newRandomGuff() {
+  var wordArray = Object.keys(words);
 
-	var randomNumber = Math.floor(Math.random() * (wordArray.length - 1)) + 1;
+  var randomNumber = Math.floor(Math.random() * (wordArray.length - 1)) + 1;
 
-	var randomWord = words[wordArray[randomNumber]];
+  while (words[wordArray[randomNumber]].slug === document.getElementsByClassName('latest-guff')[0].id) {
+    randomNumber = Math.floor(Math.random() * (wordArray.length - 1)) + 1;
+  }
 
-	var randomGuffElement = document.getElementsByClassName('random-guff')[0];
+  var randomWord = words[wordArray[randomNumber]];
 
-	var randomGuffClone = randomGuffElement.cloneNode(true);
+  var randomGuffElement = document.getElementsByClassName('random-guff')[0];
 
-	var word = randomGuffClone.children[1];
-	var details = randomGuffClone.children[2];
-	var definition = details.children[0].children[1];
-	var usageExample = details.children[1];
-	var relatedWordsElement = details.children[2];
+  var randomGuffClone = randomGuffElement.cloneNode(true);
 
-	word.href = randomWord.slug + '/';
-	word.innerHTML = randomWord.word + '&raquo;';
-	definition.innerText = randomWord.definition;
-	usageExample.innerText = randomWord.usageexample;
+  randomGuffClone.setAttribute('style', 'visibility: visible;');
 
-	if (randomWord.relatedwords) {
-		var relatedWords = randomWord.relatedwords;
-		var newRelatedWordsElement = document.createElement('p');
-		newRelatedWordsElement.className = 'detail relatedwords';
-		newRelatedWordsElement.innerHTML = 'Related: ';
+  var refreshButton = randomGuffClone.getElementsByClassName('refresh')[0];
 
-		for(var relatedWord of relatedWords) {
-			var relatedWordElement = document.createElement('a');
-			relatedWordElement.href = relatedWord.slug + '/';
-			relatedWordElement.innerHTML = relatedWord.word;
-			newRelatedWordsElement.appendChild(relatedWordElement);
-		}
-		if (relatedWordsElement) {
-			relatedWordsElement = newRelatedWordsElement;
-		} else {
-			details.appendChild(newRelatedWordsElement);
-		}
-	} else {
-		relatedWordsElement.remove()
-	}
-	randomGuffElement.innerHTML = randomGuffClone.innerHTML;
+  if (!refreshButton) {
+    refreshButton = document.createElement('span');
+    refreshButton.className = 'refresh';
+    randomGuffClone.getElementsByClassName('paraHeads')[0].appendChild(refreshButton);
+  }
 
+  refreshButton.addEventListener('click', () => {
+    newRandomGuff();
+  });
+
+  var word = randomGuffClone.getElementsByClassName('word')[0];
+  var details = randomGuffClone.getElementsByClassName('details')[0];
+  var definition = details.getElementsByClassName('detail definition')[0];
+  var usageExample = details.getElementsByClassName('detail usageexample')[0];
+  var relatedWordsElement = details.getElementsByClassName('detail relatedwords')[0];
+
+  word.href = randomWord.slug + '/';
+  word.innerHTML = randomWord.word + '&raquo;';
+
+  if (definition) {
+    details.removeChild(definition);
+  }
+  if (randomWord.definition) {
+    var newDefinitionElement = document.createElement('p');
+
+    newDefinitionElement.className = 'detail definition';
+    newDefinitionElement.innerHTML = '<strong>Translation into plain English</strong> ' + randomWord.definition;
+
+    details.appendChild(newDefinitionElement);
+  }
+
+  if (usageExample) {
+    details.removeChild(usageExample);
+  }
+  if (randomWord.usageexample) {
+    var newUsageExampleElement = document.createElement('p');
+
+    newUsageExampleElement.className = 'detail usageexample';
+    newUsageExampleElement.innerHTML = randomWord.usageexample;
+
+    details.appendChild(newUsageExampleElement);
+  }
+
+  if (relatedWordsElement) {
+    details.removeChild(relatedWordsElement);
+  }
+  if (randomWord.relatedwords.length > 0) {
+    var newRelatedWordsElement = document.createElement('p');
+
+    newRelatedWordsElement.className = 'detail relatedwords';
+    newRelatedWordsElement.innerHTML = 'Related: ';
+
+    var relatedWords = randomWord.relatedwords;
+    for (var relatedWord of relatedWords) {
+      var relatedWordElement = document.createElement('a');
+      relatedWordElement.href = relatedWord.slug + '/';
+      relatedWordElement.innerHTML = relatedWord.word;
+      if (relatedWords.indexOf(relatedWord) > 0) {
+        newRelatedWordsElement.innerHTML += ', ';
+      }
+      newRelatedWordsElement.appendChild(relatedWordElement);
+    }
+
+    details.appendChild(newRelatedWordsElement);
+  }
+  document.getElementsByTagName('main')[0].insertBefore(randomGuffClone, randomGuffElement);
+  document.getElementsByTagName('main')[0].removeChild(randomGuffElement);
 }
